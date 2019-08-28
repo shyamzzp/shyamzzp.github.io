e10e9d259f38f27e4f4639633259495a8ef02eba
shyam.suthar <shyam.suthar@kony.com>
2019-08-29 03:14:34 +0530

    Create index.php
    Co-Authored-By: Shyam SS <shyamsundersuthar009@gmail.com>

---
+++ resume/index.php
@@ -0,0 +1,1 @@
+<iframe src="https://docs.google.com/gview?url=https://raw.githubusercontent.com/shyamzzp/shyamzzp.github.io/master/resume/ShyamSS-resume.pdf&embedded=true" style="width:100%; height:98%;" frameborder="0"></iframe>
