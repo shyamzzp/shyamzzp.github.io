ce05577c492d2dcf179cbcf5b11ade5ed7c5c928
Suthar, Shyam <shyamsundersuthar009@gmail.com>
2023-06-23 13:45:11 +0530

    Remove the routes from the repo, this will be a Dashboard.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 // @ts-ignore
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./Home";
-import Projects from './Views/Projects/Projects';
-import Blog from './Views/Blog/Blog';
-import CaseStudy from './Views/CaseStudy/CaseStudy';
-import Example from './components/Small/Calendar/Calendars';
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/blog" element={<Blog />} />
-                <Route path="/casestudy" element={<CaseStudy />} />
-                <Route path="/ee" element={<Example />} />
             </Routes>
         </Router>
     );
