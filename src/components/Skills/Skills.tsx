f5d18da78aac35f8f75ac0208101b661e03329e4
Suthar, Shyam <shyamsundersuthar009@gmail.com>
2023-12-10 18:47:52 +0530

    Add more skill set to the home page.

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,50 +1,59 @@
-import React from 'react'
+import React from "react";
 
 export default function Skills() {
-    return (
-        <>
-            <div className="tags">
-                <span className="tag is-warning">Windows</span>
-                <span className="tag is-warning">Linux</span>
-            </div>
+  return (
+    <>
+      <div className="tags">
+        <span className="tag is-warning">Windows</span>
+        <span className="tag is-warning">Linux</span>
+      </div>
 
-            <div className="tags">
-                <span className="tag is-dark">Python</span>
-                <span className="tag is-dark">Java - Spring</span>
-                <span className="tag is-dark">MongoDB</span>
-                <span className="tag is-dark">NodeJS</span>
-                <span className="tag is-dark">Framework7</span>
-                <span className="tag is-dark">C# - UWP/WPF</span>
-            </div>
+      <div className="tags">
+        <span className="tag is-dark">Python</span>
+        <span className="tag is-dark">Java - Spring</span>
+        <span className="tag is-dark">MongoDB</span>
+        <span className="tag is-dark">GraphQL</span>
+        <span className="tag is-dark">NodeJS</span>
+        <span className="tag is-dark">Framework7</span>
+        <span className="tag is-dark">C# - UWP/WPF</span>
+      </div>
 
-            <div className="tags">
-                <span className="tag is-success">TypeScript</span>
-                <span className="tag is-success">Javascript</span>
-                <span className="tag is-success">NodeJS</span>
-                <span className="tag is-success">ReactJS</span>
-                <span className="tag is-success">D3(Data-Driven Documents)</span>
-                <span className="tag is-success">GraphQL</span>
-            </div>
+      <div className="tags">
+        <span className="tag is-success">TypeScript</span>
+        <span className="tag is-success">Javascript</span>
+        <span className="tag is-success">TailwindCSS</span>
+        <span className="tag is-success">NodeJS</span>
+        <span className="tag is-success">ReactJS</span>
+        <span className="tag is-success">NextJS</span>
+        <span className="tag is-success">NestJS</span>
+      </div>
 
-            <div className="tags">
-                <span className="tag is-link">Github</span>
-                <span className="tag is-link">Github - Actions/Workflows</span>
-                <span className="tag is-link">JIRA</span>
-                <span className="tag is-link">CI/CD</span>
-                <span className="tag is-link">AWS - Lambda/EC2</span>
-                <span className="tag is-link">Docker</span>
-            </div>
-            <div className="tags">
-                <span className="tag is-link">Figma (Design & Prototype)</span>
-                <span className="tag is-link">Sketch</span>
-                <span className="tag is-link">Chrome Extensions</span>
-            </div>
-            <div className="tags">
-                <span className="tag is-light-orange">RasberryPi</span>
-                <span className="tag is-light-orange">
-                    Embedded System - Arduino
-                </span>
-            </div>
-        </>
-    )
+      <div className="tags">
+        <span className="tag is-link">Github</span>
+        <span className="tag is-link">Github - Actions/Workflows</span>
+        <span className="tag is-link">JIRA</span>
+        <span className="tag is-link">CI/CD</span>
+        <span className="tag is-link">AWS - Lambda/EC2</span>
+        <span className="tag is-link">Docker</span>
+      </div>
+      <div className="tags">
+        <span className="tag is-link">Figma (Design & Prototype)</span>
+        <span className="tag is-link">Sketch</span>
+        <span className="tag is-link">Chrome Extensions</span>
+      </div>
+      <div className="tags">
+        <span className="tag is-light-grey-pink">Playwright</span>
+        <span className="tag is-light-grey-pink">NightWatch</span>
+        <span className="tag is-light-grey-pink">MQTT</span>
+        <span className="tag is-light-grey-pink">Mosquito</span>
+        <span className="tag is-light-grey-pink">
+          D3(Data-Driven Documents)
+        </span>
+      </div>
+      <div className="tags">
+        <span className="tag is-light-orange">RasberryPi</span>
+        <span className="tag is-light-orange">Embedded System - Arduino</span>
+      </div>
+    </>
+  );
 }
