f72f635548432677f5669a9269fbba5e6d712ab0
Suthar, Shyam <shyamsundersuthar009@gmail.com>
2023-12-10 19:39:39 +0530

    Remove the keys for the supabase and fix the workflow run.

--- src/Views/Glossaries/Glossaries.tsx
+++ src/Views/Glossaries/Glossaries.tsx
@@ -1,38 +1,48 @@
-import { createClient } from '@supabase/supabase-js'
-import { SUPABASE_ENDPOINT, SUPABASE_PUBLIC_ANON_KEY } from '../../config'
-import Glossary from '../../components/Glossary/Glossary'
-import { useEffect, useState } from 'react'
-import { Loader } from 'rsuite';
-import { SuffleData } from './data'
-const supabase = createClient(SUPABASE_ENDPOINT, SUPABASE_PUBLIC_ANON_KEY)
+import { createClient } from "@supabase/supabase-js";
+// import { SUPABASE_ENDPOINT, SUPABASE_PUBLIC_ANON_KEY } from '../../config'
+import Glossary from "../../components/Glossary/Glossary";
+import { useEffect, useState } from "react";
+import { Loader } from "rsuite";
+import { SuffleData } from "./data";
+// const supabase = createClient(SUPABASE_ENDPOINT, SUPABASE_PUBLIC_ANON_KEY)
 
-export default function Glossaries({setValue}:any) {
-    const [shuffledData, setShuffledData] = useState<any[]>([]);
-    const setReadMeFileContext = (data:string) => {
-        setValue(data)
-    }
-    useEffect(() => {
-        async function fetchData() {
-            const { data, error } = await supabase
-                .from('Glossary')
-                .select('*');
-            if (error) {
-                console.error(error);
-            } else {
-                setShuffledData(data);
-            }
-        }
-        fetchData();
-    }, []);
+export default function Glossaries({ setValue }: any) {
+  const [shuffledData, setShuffledData] = useState<any[]>([]);
+  const setReadMeFileContext = (data: string) => {
+    setValue(data);
+  };
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const { data, error } = await supabase.from("Glossary").select("*");
+  //     if (error) {
+  //       console.error(error);
+  //     } else {
+  //       setShuffledData(data);
+  //     }
+  //   }
+  //   fetchData();
+  // }, []);
 
-    return (
-        <div>
-            <div style={{ display: 'flex', gap: '10px', flexDirection: 'column' }} >
-                {shuffledData.length === 0?<Loader size="md" content="Fetching Data...." style={{display:'flex', justifyContent:'center'}} />:null}
-                {shuffledData.map((item) => (
-                    <Glossary key={item.id} title={item.title} reference={item.reference} level={item.level} setReadMeFileContext={setReadMeFileContext} />
-                ))}
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div style={{ display: "flex", gap: "10px", flexDirection: "column" }}>
+        {shuffledData.length === 0 ? (
+          <Loader
+            size="md"
+            content="Fetching Data...."
+            style={{ display: "flex", justifyContent: "center" }}
+          />
+        ) : null}
+        {shuffledData.map((item) => (
+          <Glossary
+            key={item.id}
+            title={item.title}
+            reference={item.reference}
+            level={item.level}
+            setReadMeFileContext={setReadMeFileContext}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
