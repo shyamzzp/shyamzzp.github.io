8cbb974ba8a4aaeca462db6a30ed599f6f12ea11
Shyam Suthar <shyamsundersuthar009@gmail.com>
2024-12-29 17:52:08 +0530

    Update the resume for the portfolio.

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/work" element={<Home />} />
       </Routes>
     </Router>
   );
---
+++ src/Work.tsx
@@ -0,0 +1,7 @@
+import "./Work.css";
+import "./bulma.min.css";
+
+function Home() {
+  return <div id="app">Hello World</div>;
+}
+export default Home;
